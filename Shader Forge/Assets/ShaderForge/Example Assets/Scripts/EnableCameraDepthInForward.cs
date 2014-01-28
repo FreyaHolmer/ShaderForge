dd0745cea3f749b8c5cae5a696f420943d759409
Joachim Holmér <webmaster@acegikmo.com>
2014-01-28 21:51:59 +0100

    Beta 0.21 - There’s now a script included for enabling camera depth on cameras

---
+++ Shader Forge/Assets/ShaderForge/Example Assets/Scripts/EnableCameraDepthInForward.cs
@@ -0,0 +1,22 @@
+﻿//
+// Attach this script to your camera in order to use depth nodes in forward rendering
+//
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class EnableCameraDepthInForward : MonoBehaviour {
+#if UNITY_EDITOR
+	void OnDrawGizmos(){
+		Set();
+	}
+#endif
+	void Start () {
+		Set();
+	}
+	void Set(){
+		if(camera.depthTextureMode == DepthTextureMode.None)
+			camera.depthTextureMode = DepthTextureMode.Depth;
+	}
+}

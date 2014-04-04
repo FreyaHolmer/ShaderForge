6a9351609a7ac42370491209ca6a912d49fbc043
Joachim Holmér <webmaster@acegikmo.com>
2014-04-04 16:44:39 +0200

    Version increased to 0.30

--- Shader Forge/Assets/ShaderForge/Editor/Code/_Utility/SF_Tools.cs
+++ Shader Forge/Assets/ShaderForge/Editor/Code/_Utility/SF_Tools.cs
@@ -25,7 +25,7 @@ namespace ShaderForge {
 
 		// Versioning
 		public static int versionNumPrimary = 0;
-		public static int versionNumSecondary = 29;
+		public static int versionNumSecondary = 30;
 		public static string versionStage = "Beta";
 		public static string version = versionNumPrimary + "." + versionNumSecondary.ToString( "D2" );
 		public static string versionString = "Shader Forge " + versionStage + " " + version;

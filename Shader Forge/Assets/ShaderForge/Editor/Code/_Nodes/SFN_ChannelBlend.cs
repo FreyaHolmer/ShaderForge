de9fc83386a469d69a9f512977a227aac6246551
Joachim Holmér <webmaster@acegikmo.com>
2014-04-12 19:24:09 +0200

    Beta 0.33 - You can now switch Channel Blend method between Summed and Layered. Summed means it will multiply all channels, and then add them together. Layered means it will blend the channels on top of each other, commonly done in terrain shaders.

--- Shader Forge/Assets/ShaderForge/Editor/Code/_Nodes/SFN_ChannelBlend.cs
+++ Shader Forge/Assets/ShaderForge/Editor/Code/_Nodes/SFN_ChannelBlend.cs
@@ -9,6 +9,9 @@ namespace ShaderForge {
 	public class SFN_ChannelBlend : SF_Node_Arithmetic {
 
 		// SF_Node tNode;
+		public enum ChannelBlendType{ Summed, Layered };
+
+		public ChannelBlendType channelBlendType = ChannelBlendType.Summed;
 
 		public SFN_ChannelBlend() {
 
@@ -17,19 +20,22 @@ namespace ShaderForge {
 
 		public override void Initialize() {
 			base.Initialize( "Channel Blend" );
-			base.PrepareArithmetic(5);
+			base.PrepareArithmetic(6);
 			base.extraWidthInput = 3;
+			base.UseLowerPropertyBox( true, true );
 
 			//SF_NodeConnection lerpCon;
 			connectors = new SF_NodeConnector[]{
-				SF_NodeConnector.Create( this, "OUT", "", 	ConType.cOutput, 	ValueType.VTvPending, false ),
-				SF_NodeConnector.Create( this, "M", "Mask", ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ).WithUseCount(4),
-				SF_NodeConnector.Create( this, "R", "Rcol", ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ),
-				SF_NodeConnector.Create( this, "G", "Gcol", ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ),
-				SF_NodeConnector.Create( this, "B", "Bcol", ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ),
-				SF_NodeConnector.Create( this, "A", "Acol", ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true )
+				SF_NodeConnector.Create( this, "OUT", "", 		ConType.cOutput, 	ValueType.VTvPending, false ),
+				SF_NodeConnector.Create( this, "M", "Mask", 	ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ).WithUseCount(4),
+				SF_NodeConnector.Create( this, "R", "Rcol", 	ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ),
+				SF_NodeConnector.Create( this, "G", "Gcol", 	ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ),
+				SF_NodeConnector.Create( this, "B", "Bcol", 	ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ),
+				SF_NodeConnector.Create( this, "A", "Acol", 	ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( true ),
+				SF_NodeConnector.Create( this, "BTM", "Btm", 	ConType.cInput, 	ValueType.VTvPending, false ).SetRequired( false )
 			};
-			base.conGroup = ScriptableObject.CreateInstance<SFNCG_ChannelBlend>().Initialize( connectors[0], connectors[1], connectors[2], connectors[3], connectors[4], connectors[5] );
+			this["BTM"].enableState = EnableState.Disabled;
+			base.conGroup = ScriptableObject.CreateInstance<SFNCG_ChannelBlend>().Initialize( connectors[0], connectors[1], connectors[2], connectors[3], connectors[4], connectors[5], connectors[6] );
 		}
 
 		public override void OnUpdateNode( NodeUpdateType updType, bool cascade = true ) {
@@ -54,12 +60,15 @@ namespace ShaderForge {
 		public override int GetEvaluatedComponentCount() {
 			return this["R"].GetCompCount();
 		}
-
+		 
 
 		private void UpdateMaskCompCountInputs(){
 
 			int cc = this["M"].GetCompCount();
-			base.texture.CompCount = cc; // Why doesn't this work?
+			base.texture.CompCount = cc;
+
+			bool summed = channelBlendType == ChannelBlendType.Summed;
+			//int enableInputCount = summed ? 4 : 5;
 
 			for(int i = 0;i<4;i++){
 
@@ -83,7 +92,31 @@ namespace ShaderForge {
 
 			}
 
+			this["BTM"].SetRequired(!summed);
+			this["BTM"].enableState = summed ? EnableState.Disabled : EnableState.Enabled;
+		}
 
+		public override void DrawLowerPropertyBox() {
+			GUI.color = Color.white;
+			EditorGUI.BeginChangeCheck();
+			channelBlendType = (ChannelBlendType)UndoableEnumPopup(lowerRect, channelBlendType, "switch channel blend type");
+			//currentUV = (UV)EditorGUI.EnumPopup( lowerRect, currentUV );
+			if(EditorGUI.EndChangeCheck()){
+				UpdateMaskCompCountInputs();
+				OnUpdateNode();
+			}
+		}
+
+		public override string SerializeSpecialData() {
+			return "chbt:" + (int)channelBlendType;
+		}
+		
+		public override void DeserializeSpecialData( string key, string value ) {
+			switch( key ) {
+			case "chbt":
+				channelBlendType = (ChannelBlendType)int.Parse( value );
+				break;
+			}
 		}
 
 
@@ -94,16 +127,31 @@ namespace ShaderForge {
 			string m = this["M"].TryEvaluate();
 
 			string[] chStr = new string[]{"r","g","b","a"};
-	
+
+
+			
 
 			string str = "(";
 
-			for(int i=0;i<cc;i++){
-				string col = connectors[i+2].TryEvaluate();
-				str += m + "." + chStr[i] + "*" + col;
-				if(i != cc-1) // not last
-					str += " + ";
+
+			if(channelBlendType == ChannelBlendType.Summed){
+				for(int i=0;i<cc;i++){
+					string col = connectors[i+2].TryEvaluate();
+					str += m + "." + chStr[i] + "*" + col;
+					if(i != cc-1) // not last
+						str += " + ";
+				}
+			} else if(channelBlendType == ChannelBlendType.Layered){
+				string inner = this["BTM"].TryEvaluateAs(GetEvaluatedComponentCount());
+				for(int i=0;i<cc;i++){
+					string col = connectors[i+2].TryEvaluate();
+					string mask = m + "." + chStr[i];
+					inner = "lerp( " + inner + ", " + col + ", " + mask + " )";
+				}
+				str += inner;
 			}
+
+
 
 			str += ")";
 
@@ -119,9 +167,17 @@ namespace ShaderForge {
 			string[] chStr = new string[]{"R","G","B","A"};
 			Color retCol = Color.black;
 
-			for(int i=0;i<cc;i++){
-				Color col = GetInputData(chStr[i])[x,y];
-				retCol += m[i] * col;
+			if(channelBlendType == ChannelBlendType.Summed){
+				for(int i=0;i<cc;i++){
+					Color col = GetInputData(chStr[i])[x,y];
+					retCol += m[i] * col;
+				}
+			} else if(channelBlendType == ChannelBlendType.Layered){
+				retCol = GetInputData("BTM")[x,y];
+				for(int i=0;i<cc;i++){
+					Color col = GetInputData(chStr[i])[x,y];
+					retCol = Color.Lerp(retCol, col, m[i]);
+				}
 			}
 
 			return retCol;

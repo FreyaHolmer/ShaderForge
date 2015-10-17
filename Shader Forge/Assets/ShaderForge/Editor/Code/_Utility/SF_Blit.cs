48934e9051e895e43676a882a84c72d1a790c81c
Joachim Holmér <webmaster@acegikmo.com>
2015-10-17 19:57:22 +0200

    1.22 - Blitting can now use tangents, normals and vertex color

--- Shader Forge/Assets/ShaderForge/Editor/Code/_Utility/SF_Blit.cs
+++ Shader Forge/Assets/ShaderForge/Editor/Code/_Utility/SF_Blit.cs
@@ -6,6 +6,92 @@ namespace ShaderForge {
 	public static class SF_Blit {
 
 
+
+		static Mesh _blitQuad;
+		public static Mesh blitQuad {
+			get {
+				if( _blitQuad == null ) {
+
+					_blitQuad = new Mesh();
+
+					Vector3[] verts = new Vector3[]{
+						new Vector3(-1,1),
+						new Vector3(1,1),
+						new Vector3(-1,-1),
+						new Vector3(1,-1)
+					};
+					Vector2[] uvs = new Vector2[]{
+						new Vector2(0,1),
+						new Vector2(1,1),
+						new Vector2(0,0),
+						new Vector2(1,0)
+					};
+					Vector3[] normals = new Vector3[]{
+						Vector3.back,
+						Vector3.back,
+						Vector3.back,
+						Vector3.back
+					};
+					Vector4[] tangents = new Vector4[]{
+						new Vector4(1,0,0,1),
+						new Vector4(1,0,0,1),
+						new Vector4(1,0,0,1),
+						new Vector4(1,0,0,1)
+					};
+					Color[] colors = new Color[]{
+						new Color(1f,0f,0f,0f),
+						new Color(0f,1f,0f,0f),
+						new Color(0f,0f,1f,0f),
+						new Color(0f,0f,0f,1f),
+					};
+
+					int[] triangles = new int[]{
+						0,1,2,
+						1,3,2
+					};
+
+					_blitQuad.vertices = verts;
+					_blitQuad.triangles = triangles;
+					_blitQuad.normals = normals;
+					_blitQuad.tangents = tangents;
+					_blitQuad.colors = colors;
+					_blitQuad.uv = uvs;
+					_blitQuad.uv2 = uvs;
+					_blitQuad.uv3 = uvs;
+					_blitQuad.uv4 = uvs;
+
+				}
+				return _blitQuad;
+			}
+		}
+
+
+
+		static GameObject blitObject;
+		public static Camera _blitCamera;
+		public static Camera blitCamera {
+			get {
+				if( blitObject == null ) {
+					blitObject = new GameObject( "SHADER_FORGE_BLIT_CAMERA" );
+					blitObject.hideFlags = HideFlags.HideAndDontSave;
+					blitObject.transform.position = new Vector3( 0, 0, -4f );
+					_blitCamera = blitObject.AddComponent<Camera>();
+					_blitCamera.enabled = false;
+					_blitCamera.clearFlags = CameraClearFlags.Nothing;
+					_blitCamera.orthographic = true;
+					_blitCamera.orthographicSize = 1f;
+					_blitCamera.nearClipPlane = 3.5f;
+					_blitCamera.farClipPlane = 4.5f;
+				}
+				return _blitCamera;
+			}
+		}
+
+
+
+
+
+
 		static string[] defaultInputNames = new string[] {
 			"_A",
 			"_B",
@@ -55,7 +141,10 @@ namespace ShaderForge {
 		}
 
 		static void LoadShaderForMaterial( string shader ) {
-			mat.shader = Shader.Find( "Hidden/Shader Forge/" + shader );
+			Shader s = Shader.Find( "Hidden/Shader Forge/" + shader );
+			if(s == null)
+				Debug.LogError("Shader not found: " + shader );
+			mat.shader = s;
 		}
 
 		public static void Render( RenderTexture target, string shader, string[] inputNames, Texture[] inputTextures ) {
@@ -75,7 +164,17 @@ namespace ShaderForge {
 
 		public static void Render( RenderTexture target, Material material ) {
 			ApplyComponentCountMask( material );
-			Graphics.Blit( null, target, material );
+
+			blitCamera.targetTexture = target;
+			Graphics.DrawMesh( blitQuad, Vector3.zero, Quaternion.identity, material, 1, blitCamera );
+			blitCamera.Render();
+			blitCamera.targetTexture = null;
+
+			//Graphics.SetRenderTarget( target );
+			//Graphics.DrawMesh( blitQuad, Matrix4x4.identity, material, -1, null );
+			//Graphics.DrawMeshNow( blitQuad, -Vector3.forward * 0.5f, Quaternion.identity );
+			
+			//Graphics.Blit( null, target, material );
 		}
 
 		static void ApplyComponentCountMask(Material material) {

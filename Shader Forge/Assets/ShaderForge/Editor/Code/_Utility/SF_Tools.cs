dcf7123cabf7d17a2c1474ab6d6fdc22ddb56e9b
Pyro_akm <pyroakm@users.noreply.github.com>
2019-01-04 14:57:21 +0100

    Updated version to 1.39.

--- Shader Forge/Assets/ShaderForge/Editor/Code/_Utility/SF_Tools.cs
+++ Shader Forge/Assets/ShaderForge/Editor/Code/_Utility/SF_Tools.cs
@@ -30,7 +30,7 @@ namespace ShaderForge {
 
 		// Versioning
 		public static int versionNumPrimary = 1;
-		public static int versionNumSecondary = 38;
+		public static int versionNumSecondary = 39;
 		public static string versionStage = "";
 		public static string version = versionNumPrimary + "." + versionNumSecondary.ToString( "D2" );
 		public static string versionString = "Shader Forge v" + version;
